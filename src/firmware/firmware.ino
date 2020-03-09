457223e8dd42ed7ec5027e8d43aa322110202519
dk9mbs <dk9mbs@heiligenhafen.the-b.eu>
2020-03-09 22:21:01 +0100

    Start

--- src/firmware/firmware.ino
+++ src/firmware/firmware.ino
@@ -1,31 +1,25 @@
-#include <Stepper.h>
+#include <CheapStepper.h>
 
-// number of steps in one revolution of your motor
-#define STEPS 2038
+CheapStepper stepper (8,9,10,11); 
 
-// set the speed in rpm
-#define MOTOR_RPM 6
-
-Stepper stepper(STEPS, 8, 10, 9, 11);
-
-String cmd;
-String dir = "R";
-int currentPos = 0;
-int minPos = 0;
-int maxPos = 4200;
-int calSteps = 100;
-
-// AZIMUT ROTOR (horizontal)
+#define CAP_MOTOR_RPM 20
 #define ROT_AZI_STEP 2
 #define ROT_AZI_DIR 3
 #define ROT_AZI_ENABLED 4
 #define ROT_AZI_LIMIT_SWITCH 5
 #define ROT_ELE_LIMIT_SWITCH 6
-
 #define ROT_STEPS_AFTER_LIMIT 1
 
-const int steps_rotate_360 = 200;
+// Cap. stepper definition
+int cap_currentPos = 0;
+int cap_minPos = 0;
+int cap_maxPos = 8500;
+int cap_calSteps = 100;
 
+// AZIMUT ROTOR (horizontal)
+int rot_azi_steps_rotate_360 = 200;
+
+//String cmd;
 
 void setup()
 {
@@ -43,15 +37,21 @@ void setup()
   Serial.println("OK -999");
   delay(1000);
 
-  commandRotorAziMz("ROT_AZI_MZ");
-  delay(1000);
+  // Invert beachten!!!
+  moveCapStepper(cap_maxPos, CAP_MOTOR_RPM);
+  Serial.println("CAP_POS:"+String(cap_currentPos));
+  moveCapStepper(cap_maxPos*-1, CAP_MOTOR_RPM);
+  Serial.println("CAP_POS:"+String(cap_currentPos));
+  commandRotorAziMz("");
 }
 
 void loop()
 {
   String result = "ERR UNKNOWN";
   String commandName = "";
-
+  //String cmd;
+  static String cmd;
+  
   if (Serial.available()) {
     char in = Serial.read();
 
@@ -72,21 +72,27 @@ void loop()
         result=commandRotorAziMz(cmd);
       }
 
-      if (commandName == "MZ") {
-        result = commandMZ(cmd);
+      if (commandName=="CAP_MAX") {
+        result = commandMoveCapAbsolute(cmd, cap_minPos);
+      }
+      if (commandName=="CAP_MIN") {
+        result = commandMoveCapAbsolute(cmd, cap_maxPos);
+      }
+      if (commandName == "CAP_ABSOLUTE") {
+        int pos = getCommandValue(cmd, ' ', 1).toInt();
+        result = commandMoveCapAbsolute(cmd, pos);
       }
 
-      if (commandName == "ML") {
-        result = commandML(cmd);
+      if (commandName == "CAP_DOWN") {
+        int steps = getCommandValue(cmd, ' ', 1).toInt();
+        result = commandCapMove(cmd,steps, "DOWN");
       }
 
-      if (commandName == "MR") {
-        result = commandMR(cmd);
+      if (commandName == "CAP_UP") {
+        int steps = getCommandValue(cmd, ' ', 1).toInt();
+        result = commandCapMove(cmd,steps, "UP");
       }
 
-      if (commandName == "MA") {
-        result = commandAbsolute(cmd);
-      }
 
       if (commandName == "SZ") {
         result = commandSZ(cmd);
@@ -97,9 +103,9 @@ void loop()
       }
 
       if (commandName == "CAL") {
-        result = commandCal(cmd);
+        result = commandCapCal(cmd);
       }
-      Serial.println("CAP_POS:"+String(currentPos));
+      Serial.println("CAP_POS:"+String(cap_currentPos));
       Serial.println("ROT_AZI_POS:0");
       Serial.println("LAST_CMD:"+cmd);
       Serial.println(result);
@@ -139,81 +145,70 @@ String commandRotorAziMz(String cmd) {
   return "OK";
 }
 
-String commandCal(String cmd) {
-  moveStepper(calSteps * -1, MOTOR_RPM);
+String commandCapCal(String cmd) {
+  moveCapStepper(cap_calSteps * -1, CAP_MOTOR_RPM);
   return "OK";
 }
 
 String commandSP(String cmd) {
-  return "OK " + String(currentPos);
+  return "OK " + String(cap_currentPos);
 }
 
 String commandSZ(String cmd) {
-  currentPos = 0;
+  cap_currentPos = 0;
   return "OK";
 }
 
-String commandMZ(String cmd) {
-  if (currentPos > 0) {
-    moveStepper(currentPos * -1, MOTOR_RPM);
-  }
-  currentPos = 0;
-  return "OK "+String(currentPos);
-}
-
-String commandAbsolute(String cmd) {
-  int pos = getCommandValue(cmd, ' ', 1).toInt();
+String commandMoveCapAbsolute(String cmd, int pos) {
   int steps = 0;
 
-  steps = pos - currentPos;
+  steps = pos - cap_currentPos;
 
-  if (steps + currentPos < minPos) steps = 0;
-  if (steps + currentPos > maxPos) steps = maxPos;
+  if (steps + cap_currentPos < cap_minPos) steps = 0;
+  if (steps + cap_currentPos > cap_maxPos) steps = cap_maxPos;
 
-  currentPos += steps;
-
-  moveStepper(steps, MOTOR_RPM);
-  return "OK "+String(currentPos);
+  moveCapStepper(steps, CAP_MOTOR_RPM);
+  return "OK "+String(cap_currentPos);
 }
 
-String commandML(String cmd) {
-  int steps = getCommandValue(cmd, ' ', 1).toInt();
-
-  if (currentPos + steps > maxPos) {
-    steps = maxPos - currentPos;
+String commandCapMove(String cmd, int steps, String dir) {
+  if (dir == "DOWN" && cap_currentPos + steps > cap_maxPos) {
+    steps = cap_maxPos - cap_currentPos;
   }
 
-  currentPos += steps;
-  moveStepper(steps, MOTOR_RPM);
-  return "OK "+String(currentPos);
-}
-
-String commandMR(String cmd) {
-  int steps = getCommandValue(cmd, ' ', 1).toInt();
-
-  if (currentPos - steps < minPos) {
-    steps = currentPos;
+  if (dir == "UP" && cap_currentPos - steps < cap_minPos) {
+    steps = cap_currentPos;
   }
 
-  currentPos -= steps;
-  moveStepper(steps * -1, MOTOR_RPM);
-  return "OK "+String(currentPos);
+  if (dir == "UP") {
+    steps=steps*-1;
+  }
+
+  moveCapStepper(steps, CAP_MOTOR_RPM);
+  return "OK "+String(cap_currentPos);
 }
+
 
 /*
  * 
  * Helper functions
  * 
  */
-void moveStepper(int steps, int rpm) {
+void moveCapStepper(int steps, int rpm) {
+  cap_currentPos += steps;
+
+  stepper.setRpm(rpm);
+  bool moveClockwise;
   if (steps < 0) {
-    dir = "R";
+    moveClockwise = false;
+    steps=steps*-1;
   } else {
-    dir = "L";
+    moveClockwise = true;
   }
 
-  stepper.setSpeed(rpm);
-  stepper.step(steps);
+  for (int s=0; s<steps; s++){
+    stepper.step(moveClockwise);
+  }
 
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
@@ -231,7 +226,7 @@ void moveRotorStepperAzi(String dir, int steps , int readLimitSwitch) {
   }
   
   digitalWrite(ROT_AZI_ENABLED, LOW);
-  for(int i = 0; i < steps_rotate_360*steps; i++) {
+  for(int i = 0; i < rot_azi_steps_rotate_360*steps; i++) {
     
     if(digitalRead(ROT_AZI_LIMIT_SWITCH) == HIGH && readLimitSwitch==1){
       delay(10);
